4f6ec9f3f907183ea6a27da5248d24795592e3d7
ZontaRok <76939741+ZontaRok@users.noreply.github.com>
2024-01-21 14:16:46 +0100

    LeaderBoard

--- Assets/AudioLoudnessDetection.cs
+++ Assets/AudioLoudnessDetection.cs
@@ -4,101 +4,59 @@ using UnityEngine;
 
 public class AudioLoudnessDetection : MonoBehaviour
 {
-        
-    //public float loudness = 0;
-    //public float loudnessThreshold = 0.5f;
-    //public float sensitivity = 100;
-    //public float moveSpeed = 0.1f;
 
-    //public float minY = -2.0f;
-    //public float maxY = 2.0f
+    public float loudness = 0;
+    public float loudnessThreshold = 0.5f;
+    public float sensitivity = 100;
+    public float moveSpeed = 0.1f;
 
-    public int samapleWindows = 64;
-    private AudioClip microphoneClip;
+    public float minY = -2.0f;
+    public float maxY = 2.0f;
 
 
-    //AudioSource audioSource;
-
-    //void Start()
-    //{
-    //    audioSource = GetComponent<AudioSource>();
-    //    audioSource.clip = Microphone.Start(null, true, 1, 44100);
-    //    audioSource.loop = true;
-    //    while (!(Microphone.GetPosition(null) > 0)) { }
-    //    audioSource.Play();
-    //}
-
-
-
-    //void Update()
-    //{
-    //    loudness = GetAverageVolume() * sensitivity;
-
-    //    float translationSpeed = Mathf.Lerp(0, moveSpeed, loudness / loudnessThreshold);
-
-    //    if (loudness > loudnessThreshold)
-    //    {
-    //        transform.Translate(Vector3.up * translationSpeed * Time.deltaTime);
-    //    }
-    //    else
-    //    {
-    //        transform.Translate(Vector3.down * translationSpeed * Time.deltaTime);
-    //    }
-
-    //    float newY = Mathf.Clamp(transform.position.y, minY, maxY);
-    //    transform.position = new Vector3(transform.position.x, newY, transform.position.z);
-    //}
-
-
-    //float GetAverageVolume()
-    //{
-    //    float[] data = new float[64];
-    //    float a = 0;
-    //    audioSource.GetOutputData(data, 0);
-    //    foreach (float s in data)
-    //    {
-    //        a += Mathf.Abs(s);
-    //    }
-
-    //    return (a / data.Length);
-    //}
+    AudioSource audioSource;
 
     void Start()
     {
-        MicrophoneToAudioClip();
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = Microphone.Start(null, true, 1, 44100);
+        audioSource.loop = true;
+        while (!(Microphone.GetPosition(null) > 0)) { }
+        audioSource.Play();
     }
 
-    // Update is called once per frame
+
+
     void Update()
     {
+        loudness = GetAverageVolume() * sensitivity;
 
-    }
+        float translationSpeed = Mathf.Lerp(0, moveSpeed, loudness / loudnessThreshold);
 
-    public float GetLoudnessfromMicrophone()
-    {
-        return GetLoudnessfromAudioClip(Microphone.GetPosition(Microphone.devices[0]), microphoneClip);
-    }
-
-    public  void MicrophoneToAudioClip()
-    {
-        string microphoneName = Microphone.devices[0];
-        microphoneClip = Microphone.Start(microphoneName, true, 20, AudioSettings.outputSampleRate);
-    }
-
-    public float GetLoudnessfromAudioClip(int clipPosition, AudioClip clip)
-    {
-        int startPosition = clipPosition - samapleWindows;
-
-        float[] waveDate = new float[samapleWindows];
-        clip.GetData(waveDate, startPosition);
-
-        float totalLoudness = 0;
-
-        for(int i = 0; i < samapleWindows; i++)
+        if (loudness > loudnessThreshold)
         {
-            totalLoudness += Mathf.Abs(waveDate[i]);
+            transform.Translate(Vector3.up * translationSpeed * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(Vector3.down * translationSpeed * Time.deltaTime);
         }
 
-        return totalLoudness / samapleWindows;  
+        float newY = Mathf.Clamp(transform.position.y, minY, maxY);
+        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+    }
+
+
+    float GetAverageVolume()
+    {
+        float[] data = new float[64];
+        float a = 0;
+        audioSource.GetOutputData(data, 0);
+        foreach (float s in data)
+        {
+            a += Mathf.Abs(s);
+        }
+
+        return (a / data.Length);
     }
 }
